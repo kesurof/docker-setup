305ea021fa3a227dab20c585612eb6ab64653701
kesurof <64525827+kesurof@users.noreply.github.com>
2023-09-07 16:29:10 +0200

    update

--- script.sh
+++ script.sh
@@ -4,9 +4,9 @@
 env_file="/home/$USER/.env"
 
 # Fonction pour afficher une question en jaune
-    function ask_question() {
-        echo -e "\033[33m$1\033[0m"
-    }
+function ask_question() {
+    echo -e "\033[33m$1\033[0m"
+}
 
 # Fonction pour afficher une question en jaune et stocker la réponse dans le fichier .env
 function ask_question_and_store_in_env() {
@@ -22,151 +22,139 @@ if [[ ! -f "$env_file" ]]; then
     touch "$env_file"
 fi
 
-    # Vérifier si l'utilisateur a les droits sudo
-    if [ "$(id -u)" -eq 0 ]; then
-        echo "Ce script ne doit pas être exécuté en tant que superutilisateur (root). Utilisez un utilisateur avec les droits sudo."
+# Vérifier si l'utilisateur a les droits sudo
+if [ "$(id -u)" -eq 0 ]; then
+    echo "Ce script ne doit pas être exécuté en tant que superutilisateur (root). Utilisez un utilisateur avec les droits sudo."
+    exit 1
+fi
+
+# Vérifier si l'utilisateur appartient au groupe Docker
+if ! groups "$USER" | grep &>/dev/null '\bdocker\b'; then
+    echo "L'utilisateur n'appartient pas au groupe Docker. Ajout de l'utilisateur au groupe Docker..."
+    sudo usermod -aG docker "$USER"
+    if [ $? -eq 0 ]; then
+        echo "L'utilisateur a été ajouté au groupe Docker avec succès. Veuillez vous déconnecter/reconnecter et relancer le script 'instal.sh' pour continuer."
+        exit 1
+    else
+        echo "Impossible d'ajouter l'utilisateur au groupe Docker. Veuillez le faire manuellement et réexécutez le script."
         exit 1
     fi
+fi
 
-    # Vérifier si l'utilisateur appartient au groupe Docker
-    if ! groups "$USER" | grep &>/dev/null '\bdocker\b'; then
-        echo "L'utilisateur n'appartient pas au groupe Docker. Ajout de l'utilisateur au groupe Docker..."
-        sudo usermod -aG docker "$USER"
-        if [ $? -eq 0 ]; then
-            echo "L'utilisateur a été ajouté au groupe Docker avec succès. Veuillez vous déconnecter/reconnecter et relancer le script 'instal.sh' pour continuer."
-            exit 1
-        else
-            echo "Impossible d'ajouter l'utilisateur au groupe Docker. Veuillez le faire manuellement et réexécutez le script."
-            exit 1
-        fi
-    fi
-
-    # Fonction pour demander à l'utilisateur de continuer ou d'annuler
-    function ask_to_continue() {
-        ask_question "Voulez-vous continuer l'installation ? (Oui/Non) "
-        read continue_choice
-        if [ "$continue_choice" != "oui" ] && [ "$continue_choice" != "Oui" ] && [ "$continue_choice" != "o" ] && [ "$continue_choice" != "O" ]; then
-            echo "Installation annulée."
-            exit 1
-        fi
-    }
-
-    # Vérifier la distribution Linux en cours d'exécution
-    distro=$(lsb_release -si)
-    version=$(lsb_release -sr)
-
-    if [ "$distro" != "Ubuntu" ] && [ "$distro" != "Debian" ]; then
-        echo "Ce script ne prend en charge que les distributions Ubuntu et Debian."
+# Fonction pour demander à l'utilisateur de continuer ou d'annuler
+function ask_to_continue() {
+    ask_question "Voulez-vous continuer l'installation ? (Oui/Non) "
+    read continue_choice
+    if [ "$continue_choice" != "oui" ] && [ "$continue_choice" != "Oui" ] && [ "$continue_choice" != "o" ] && [ "$continue_choice" != "O" ]; then
+        echo "Installation annulée."
         exit 1
     fi
+}
 
-    # Adapter les sources Apt en fonction de la distribution et de la version
-    if [ "$distro" == "Ubuntu" ]; then
-        if [ "$version" == "20.04" ] || [ "$version" == "22.04" ]; then
-            docker_repo="https://download.docker.com/linux/ubuntu"
-        else
-            echo "La version Ubuntu $version n'est pas prise en charge."
-            exit 1
-        fi
-    elif [ "$distro" == "Debian" ]; then
-        if [ "$version" == "11" ] || [ "$version" == "12" ]; then
-            docker_repo="https://download.docker.com/linux/debian"
-        else
-            echo "La version Debian $version n'est pas prise en charge."
-            exit 1
-        fi
+# Vérifier la distribution Linux en cours d'exécution
+distro=$(lsb_release -si)
+version=$(lsb_release -sr)
+
+if [ "$distro" != "Ubuntu" ] && [ "$distro" != "Debian" ]; then
+    echo "Ce script ne prend en charge que les distributions Ubuntu et Debian."
+    exit 1
+fi
+
+# Adapter les sources Apt en fonction de la distribution et de la version
+if [ "$distro" == "Ubuntu" ]; then
+    if [ "$version" == "20.04" ] || [ "$version" == "22.04" ]; then
+        docker_repo="https://download.docker.com/linux/ubuntu"
+    else
+        echo "La version Ubuntu $version n'est pas prise en charge."
+        exit 1
     fi
-
-    # Demander à l'utilisateur le chemin d'installation des volumes des containers (par défaut /home/$USER/seedbox/app_settings)
-    ask_question "Veuillez entrer le chemin d'installation des volumes des containers (par défaut /home/$USER/seedbox/app_settings) : "
-    read container_volumes_path
-
-    # Utiliser le chemin par défaut si l'utilisateur n'a rien saisi
-    if [ -z "$container_volumes_path" ]; then
-        container_volumes_path="/home/$USER/seedbox/app_settings"
+elif [ "$distro" == "Debian" ]; then
+    if [ "$version" == "11" ] || [ "$version" == "12" ]; then
+        docker_repo="https://download.docker.com/linux/debian"
+    else
+        echo "La version Debian $version n'est pas prise en charge."
+        exit 1
     fi
+fi
+
+# Demander à l'utilisateur le chemin d'installation des volumes des containers (par défaut /home/$USER/seedbox/app_settings)
+ask_question "Veuillez entrer le chemin d'installation des volumes des containers (par défaut /home/$USER/seedbox/app_settings) : "
+read container_volumes_path
+
+# Utiliser le chemin par défaut si l'utilisateur n'a rien saisi
+if [ -z "$container_volumes_path" ]; then
+    container_volumes_path="/home/$USER/seedbox/app_settings"
+fi
+
+# Installation de Docker
+ask_question "Voulez-vous installer Docker ? (Oui/Non) "
+read choix
+
+if [ "$choix" = "oui" ] || [ "$choix" = "Oui" ] || [ "$choix" = "o" ] || [ "$choix" = "O" ]; then
+    # Ajout de la clé GPG officielle de Docker
+    sudo apt-get update
+    sudo apt-get install -y ca-certificates curl gnupg
+    sudo install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL $docker_repo/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+    # Ajout du référentiel Docker aux sources Apt
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] $docker_repo $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    # Mise à jour des informations du package
+    sudo apt-get update
 
     # Installation de Docker
-    ask_question "Voulez-vous installer Docker ? (Oui/Non) "
-    read choix
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
-    if [ "$choix" = "oui" ] || [ "$choix" = "Oui" ] || [ "$choix" = "o" ] || [ "$choix" = "O" ]; then
-        # Ajout de la clé GPG officielle de Docker
-        sudo apt-get update
-        sudo apt-get install -y ca-certificates curl gnupg
-        sudo install -m 0755 -d /etc/apt/keyrings
-        curl -fsSL $docker_repo/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-        sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    # Installation de Docker Compose
+    echo "Installation de Docker Compose..."
+    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
 
-        # Ajout du référentiel Docker aux sources Apt
-        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] $docker_repo $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    # Vérification de l'installation de Docker et Docker Compose
+    sudo docker --version
+    sudo docker-compose --version
 
-        # Mise à jour des informations du package
-        sudo apt-get update
+    echo "Docker et Docker Compose ont été installés avec succès."
+fi
 
-        # Installation de Docker
-        sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+# Chemin complet pour enregistrer le fichier wg0.conf
+container_volumes_path="/home/$USER/seedbox/app_settings"  # Définissez le chemin ici
+wg0_config_path="$container_volumes_path/wireguard/config/wg0.conf"
 
-        # Installation de Docker Compose
-        echo "Installation de Docker Compose..."
-        sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-        sudo chmod +x /usr/local/bin/docker-compose
-
-        # Vérification de l'installation de Docker et Docker Compose
-        sudo docker --version
-        sudo docker-compose --version
-
-        echo "Docker et Docker Compose ont été installés avec succès."
+# Vérifier si le répertoire $container_volumes_path/wireguard/config existe, sinon le créer
+if [ ! -d "$container_volumes_path/wireguard/config" ]; then
+    ask_question "Le répertoire $container_volumes_path/wireguard/config n'existe pas. Voulez-vous le créer ? (Oui/Non) "
+    read create_dir_choice
+    if [ "$create_dir_choice" = "oui" ] || [ "$create_dir_choice" = "Oui" ] || [ "$create_dir_choice" = "o" ] || [ "$create_dir_choice" = "O" ]; then
+        mkdir -p "$container_volumes_path/wireguard/config"
+        echo "Le répertoire $container_volumes_path/wireguard/config a été créé."
+    else
+        echo "Le répertoire $container_volumes_path/wireguard/config n'a pas été créé. Le fichier WireGuard ne sera pas enregistré."
+        exit 1
     fi
+fi
 
-    #!/bin/bash
+# Demander à l'utilisateur de coller le code de configuration WireGuard
+ask_question "Veuillez coller le code de configuration WireGuard ci-dessous (appuyez sur Entrée puis Ctrl+D pour terminer) : "
+wireguard_config=""
+while IFS= read -r line; do
+    wireguard_config+="$line\n"
+done
 
-    # Chemin complet pour enregistrer le fichier wg0.conf
-    container_volumes_path="/home/$USER/seedbox/app_settings"  # Définissez le chemin ici
-    wg0_config_path="$container_volumes_path/wireguard/config/wg0.conf"
+# Enregistrez le code de configuration WireGuard dans le fichier
+echo -e "$wireguard_config" > "$wg0_config_path"
+echo "Le code de configuration WireGuard a été enregistré dans $wg0_config_path."
 
-    # Vérifier si le répertoire $container_volumes_path/wireguard/config existe, sinon le créer
-    if [ ! -d "$container_volumes_path/wireguard/config" ]; then
-        ask_question "Le répertoire $container_volumes_path/wireguard/config n'existe pas. Voulez-vous le créer ? (Oui/Non) "
-        read create_dir_choice
-        if [ "$create_dir_choice" = "oui" ] || [ "$create_dir_choice" = "Oui" ] || [ "$create_dir_choice" = "o" ] || [ "$create_dir_choice" = "O" ]; then
-            mkdir -p "$container_volumes_path/wireguard/config"
-            echo "Le répertoire $container_volumes_path/wireguard/config a été créé."
-        else
-            echo "Le répertoire $container_volumes_path/wireguard/config n'a pas été créé. Le fichier WireGuard ne sera pas enregistré."
-            exit 1
-        fi
-    fi
+# Poser les questions et stocker les réponses dans le fichier .env
+ask_question_and_store_in_env "Veuillez entrer votre clé API RealDebrid : " RD_API_KEY
+ask_question_and_store_in_env "Veuillez entrer le nom de domaine ou l'adresse IP du serveur Plex : " PLEX_ADDRESS
+ask_question_and_store_in_env "Veuillez entrer votre identifiant Plex : " PLEX_USER
+ask_question_and_store_in_env "Veuillez entrer votre claim Plex (https://www.plex.tv/claim/): " PLEX_TOKEN
 
-    # Demander à l'utilisateur de coller le code de configuration WireGuard
-    ask_question "Veuillez coller le code de configuration WireGuard ci-dessous (appuyez sur Entrée puis Ctrl+D pour terminer) : "
-    wireguard_config=""
-    while IFS= read -r line; do
-        wireguard_config+="$line\n"
-    done
-
-    # Enregistrez le code de configuration WireGuard dans le fichier
-    echo -e "$wireguard_config" > "$wg0_config_path"
-    echo "Le code de configuration WireGuard a été enregistré dans $wg0_config_path."
-
-    # Fonction pour afficher une question en jaune et stocker la réponse dans le fichier .env
-    function ask_question_and_store_in_env() {
-        local question="$1"
-        local env_var_name="$2"
-        echo -e "\033[33m$question\033[0m"
-        read answer
-        echo "$env_var_name=\"$answer\"" >> "/home/$USER/.env"
-    }
-
-    # Poser les questions et stocker les réponses dans le fichier .env
-    ask_question_and_store_in_env "Veuillez entrer votre clé API RealDebrid : " RD_API_KEY
-    ask_question_and_store_in_env "Veuillez entrer le nom de domaine ou l'adresse IP du serveur Plex : " PLEX_ADDRESS
-    ask_question_and_store_in_env "Veuillez entrer votre identifiant Plex : " PLEX_USER
-    ask_question_and_store_in_env "Veuillez entrer votre claim Plex (https://www.plex.tv/claim/): " PLEX_TOKEN
-
-
-    # Génération du fichier docker-compose.yml avec la clé API RealDebrid, l'adresse du serveur Plex, l'identifiant Plex et le token Plex
-    cat <<EOL > docker-compose.yml
+# Génération du fichier docker-compose.yml avec la clé API RealDebrid, l'adresse du serveur Plex, l'identifiant Plex et le token Plex
+cat <<EOL > docker-compose.yml
 version: '3'
 
 services:
@@ -199,7 +187,7 @@ services:
       - VERSION=docker
       - PUID=1000
       - PGID=1000
-      - PLEX_CLAIM=$plex_token
+      - PLEX_CLAIM=$PLEX_TOKEN
       - TZ=Europe/Paris
     network_mode: host
     volumes:
@@ -226,12 +214,12 @@ services:
       - /home/$USER/seedbox/rclone:/data:shared
     environment:
       - TZ=Europe/Paris
-      - RD_API_KEY=$rd_api_key
+      - RD_API_KEY=$RD_API_KEY
       - RCLONE_MOUNT_NAME=realdebrid
       - RCLONE_DIR_CACHE_TIME=10s
-      - PLEX_USER=$plex_user
-      - PLEX_TOKEN=$plex_token
-      - PLEX_ADDRESS=$plex_address
+      - PLEX_USER=$PLEX_USER
+      - PLEX_TOKEN=$PLEX_TOKEN
+      - PLEX_ADDRESS=$PLEX_ADDRESS
       - SHOW_MENU=false
     devices:
       - /dev/fuse:/dev/fuse:rwm
