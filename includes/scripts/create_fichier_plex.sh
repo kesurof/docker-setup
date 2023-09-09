a56172d74796c78ef5970b1dba42f1e2dff89b9b
kesurof <64525827+kesurof@users.noreply.github.com>
2023-09-09 14:30:42 +0200

    update

--- includes/scripts/create_fichier_plex.sh
+++ includes/scripts/create_fichier_plex.sh
@@ -25,6 +25,18 @@ load_env_variables "$env_file"
 
 # À partir de ce point, toutes les variables du fichier .env sont disponibles, y compris $FOLDER_APP_SETTINGS
 
+# Chemin par défaut pour le dossier à changer de propriétaire
+folder_to_change_owner="$FOLDER_RCLONE"
+
+# Récupérer le nom d'utilisateur actuellement connecté
+current_user=$(whoami)
+
+# Changer le propriétaire du dossier récursivement
+chown -R "$current_user:$current_user" "$folder_to_change_owner"
+
+# Afficher un message de confirmation
+echo "Le propriétaire du dossier $folder_to_change_owner a été changé à $current_user."
+
 # Chemin vers les dossiers
 create_fichier_shows="$FOLDER_RCLONE/realdebrid/shows/"
 create_fichier_movies="$FOLDER_RCLONE/realdebrid/movies/"
@@ -53,15 +65,3 @@ for dossier in "$create_fichier_shows" "$create_fichier_movies" "$create_fichier
   # Utiliser 'dd' pour créer un fichier vide de la taille spécifiée
   dd if=/dev/zero of="$chemin_fichier" bs="$taille_octets" count=1
 done
-
-# Chemin par défaut pour le dossier à changer de propriétaire
-folder_to_change_owner="$FOLDER_RCLONE"
-
-# Récupérer le nom d'utilisateur actuellement connecté
-current_user=$(whoami)
-
-# Changer le propriétaire du dossier récursivement
-chown -R "$current_user:$current_user" "$folder_to_change_owner"
-
-# Afficher un message de confirmation
-echo "Le propriétaire du dossier $folder_to_change_owner a été changé à $current_user."
