b2fa100b730ea44773b919b89eb44b2bbc91b257
kesurof <64525827+kesurof@users.noreply.github.com>
2023-09-25 15:53:29 +0200

    update

--- includes/scripts/generator_yml.sh
+++ includes/scripts/generator_yml.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Fonction pour afficher une question en jaune
+ask_question() {
+  echo -e "\033[33m$1\033[0m"
+}
+
 # Chemin par défaut pour le fichier .env
 env_file_path="/home/$(logname)"
 env_file="$env_file_path/.env"
 
 # Fonction pour charger toutes les variables depuis le fichier .env
-function load_env_variables() {
+load_env_variables() {
   local env_file="$1"
   if [ -f "$env_file" ]; then
     source "$env_file"
@@ -18,34 +23,25 @@ function load_env_variables() {
 # Charger toutes les variables depuis le fichier .env
 load_env_variables "$env_file"
 
-# Vérifier si le fichier .env existe
-if [ ! -f "$env_file" ]; then
-  echo "Le fichier .env n'existe pas. Veuillez d'abord créer le fichier .env avec les données nécessaires."
-  exit 1
-fi
+# À partir de ce point, toutes les variables du fichier .env sont disponibles, y compris $APP_SETTINGS_DIR
 
-# Charger les variables du fichier .env
-source "$env_file"
+# Chemin vers le fichier docker-compose.yml
+docker_compose_file="$APP_SETTINGS_DIR/docker-compose.yml"
 
-# Générer le docker-compose.yml
-cat <<EOL > docker-compose.yml
-version: '3'
-services:
-  myapp:
-    image: myapp-image
-    environment:
-      USER_HOME: "$USER_HOME"
-      LOCAL_DIR: "$LOCAL_DIR"
-      MEDIAS_DIR: "$MEDIAS_DIR"
-      APP_SETTINGS_DIR: "$APP_SETTINGS_DIR"
-      RCLONE_DIR: "$RCLONE_DIR"
-      RCLONE_CONFIG_FILE: "$RCLONE_CONFIG_FILE"
-      RD_API_KEY: "$RD_API_KEY"
-      RD_TOKEN_PLEX: "$RD_TOKEN_PLEX"
-      PLEX_ADDRESS: "$PLEX_ADDRESS"
-      PLEX_USER: "$PLEX_USER"
-      PLEX_PASSWD: "$PLEX_PASSWD"
-      PLEX_CLAIM: "$PLEX_CLAIM"
-EOL
+# Chemin du répertoire où sera copié le fichier docker-compose.yml
+app_settings_dir="/home/$(logname)/seedbox/app_settings"
 
-echo "Le fichier docker-compose.yml a été généré avec succès."
+# Copier le contenu du fichier includes/templates/docker-compose.yml vers $app_settings_dir
+cp includes/templates/docker-compose.yml "$app_settings_dir"
+
+# Remplacer les variables dans docker-compose.yml en utilisant les valeurs du .env
+env_vars=$(grep -oE '\{\{[A-Za-z_][A-Za-z_0-9]*\}\}' "$app_settings_dir/docker-compose.yml")
+
+for var in $env_vars; do
+  var_name=$(echo "$var" | sed 's/[{}]//g')
+  var_value=$(grep "^$var_name=" "$env_file" | cut -d'=' -f2)
+  sed -i "s|{{${var_name}}}|${var_value}|g" "$app_settings_dir/docker-compose.yml"
+done
+
+# Afficher un message
+echo -e "\033[32mLes informations ont été ajoutées au fichier docker-compose.yml.\033[0m"
