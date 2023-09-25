021b5adf3a4b18f4f45fef82bbb2af72e3d265db
kesurof <64525827+kesurof@users.noreply.github.com>
2023-09-25 15:49:45 +0200

    update

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ scripts_dir="$(dirname "$0")/includes/scripts"
 scripts=("install_full.sh" "install_docker.sh" "config_setup.sh" "conf_wireguard.sh" "install_container.sh" "install_rclone.sh" "generator_yml.sh")
 
 # Noms personnalisés pour les scripts
-script_names=("Installation complète" "Installation de docker" "Personnaliser docker-compose.yml" "Créer .conf de Wireguard" "Installation des containers" "Installation de Rclone-RD" "Générateur YML")
+script_names=("Installation complète" "Installation de docker" "Config Setup variables" "Créer .conf de Wireguard" "Installation des containers" "Installation de Rclone-RD" "Générateur YML")
 
 # Chemin complet vers les scripts
 script_paths=()
