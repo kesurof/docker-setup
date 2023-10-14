ca9f82ea1682841b1125d563e38999e54d6c7adb
laster13 <laster13012@gmail.com>
2023-10-14 10:41:10 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,127 +1,57 @@
 #!/bin/bash
 
-# Répertoire complet où se trouvent les scripts (à la racine)
-scripts_dir="$(dirname "$0")/includes/scripts"
+export NEWT_COLORS='
+  window=,white
+  border=green,blue
+  textbox=black,white
+'
+# Absolute path to this script.
+CURRENT_SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in.
+SETTINGS_SOURCE=$(dirname "$CURRENT_SCRIPT")
+export SETTINGS_SOURCE
 
-# Liste des noms de scripts à exécuter dans l'ordre spécifié
-scripts=("install_full.sh" "install_docker.sh" "config_setup.sh" "conf_wireguard.sh" "install_container.sh" "install_rclone.sh" "generator_yml.sh")
+env="/home/$USER/.env"
+if [ -f "$env" ]; then
 
-# Noms personnalisés pour les scripts
-script_names=("Installation complète" "Installation de docker" "Config Setup variables" "Créer .conf de Wireguard" "Installation des containers" "Installation de Rclone-RD" "Générateur YML")
+  # Chemin par défaut pour le fichier .env
+  env_file_path="/home/$USER"
+  env_file="$env_file_path/.env"
 
-# Chemin complet vers les scripts
-script_paths=()
-for script in "${scripts[@]}"; do
-    script_paths+=("$scripts_dir/$script")
-done
-
-# Fonction pour afficher le menu
-afficher_menu() {
-    clear
-    plex_debrid
-    echo -e "\e[1;33mMenu d'options :\e[0m"
-    for i in "${!script_names[@]}"; do
-        echo -e "\e[1;33m$((i + 1)).\e[0m \e[1;32m${script_names[i]}.\e[0m"
-    done
+  # Fonction pour charger toutes les variables depuis le fichier .env
+  function load_env_variables() {
+    local env_file="$1"
+    if [ -f "$env_file" ]; then
+      source "$env_file"
+    else
+      echo "Le fichier .env n'a pas été trouvé à $env_file. Assurez-vous qu'il existe avant de continuer."
+      exit 1
+    fi
 }
 
-plex_debrid() {
-text="Plex Debrid"
-colors=("36") # Couleurs ANSI
-reset_color="\e[0m"
+  # Charger toutes les variables depuis le fichier .env
+  load_env_variables "$env_file"
 
-for color in "${colors[@]}"; do
-  echo -e "\e[${color}m$(figlet -f slant "$text")${reset_color}"
-done
-}
 
-# Fonction pour exécuter un script
-executer_script() {
-    script_path="$1"
-    echo -e "\e[1;33mExécution de $script_path :\e[0m"
-    "$script_path"
-    read -p "Appuyez sur Entrée pour revenir au menu..."
-}
+  # création d'un virtualenv
+   python3 -m venv ${SETTINGS_SOURCE}/venv
 
-function main_menu {
-  while true; do
-    clear
-    plex_debrid
-    echo -e "\e[1;33mMenu\e[0m"
-
-    echo -e "\e[1;32m 1. Installation *arr + rclone + rdtclient + plex \e[0m"
-    echo -e "\e[1;32m 2. Installation Plex debrid \e[0m"
-    echo -e "\e[1;32m 3. Quitter \e[0m"
-
-    read -p "Entrer votre choix: " choice
-
-    case $choice in
-        1)
-          # Rendre les scripts exécutables s'ils ne le sont pas déjà
-          for script_path in "${script_paths[@]}"; do
-              if [ ! -x "$script_path" ]; then
-                chmod +x "$script_path"
-              fi
-          done
-
-          # Boucle principale du menu
-          while true; do
-              afficher_menu
-              read -p "Sélectionnez une option (1-${#script_names[@]}, Q pour revenir au menu général) : " choix
-              if [ "$choix" == "Q" ] || [ "$choix" == "q" ]; then
-                main_menu
-              fi
-
-              if [[ "$choix" =~ ^[0-9]+$ ]] && [ "$choix" -ge 1 ] && [ "$choix" -le ${#script_names[@]} ]; then
-                index=$((choix - 1))
-                executer_script "${script_paths[index]}"
-              else
-                echo -e "\e[31mOption invalide. Veuillez sélectionner une option valide.\e[0m"
-                read -p "Appuyez sur Entrée pour continuer..."
-              fi
-          done
-          ;;
-
-        2)
-          # Liste des fichiers à afficher dans le menu
-          scripts=("install_docker.sh" "config_setup.sh" "conf_wireguard.sh" "install_applis.sh" "install_rclone.sh" "install_plex_debrid.sh")
+  # activation du venv
+  source ${SETTINGS_SOURCE}/venv/bin/activate
+  source "${SETTINGS_SOURCE}/includes/scripts/functions.sh"
  
-          # Noms personnalisés pour les scripts
-          script_names=("Installation de docker" "Config Setup variables" "Créer .conf de Wireguard" "Installation des applis" "Installation de Rclone-RD" "Installation de Plex_Debrid" )
+  #lancement menu
+  main_menu
 
-          # Afficher un menu interactif
-          while true; do
-            clear
+elif [ -e "/usr/bin/docker" ]; then
+  #lancement menu
+  source "${SETTINGS_SOURCE}/includes/scripts/functions.sh"
+  main_menu
 
+else
 
-            # Afficher les fichiers disponibles
-            clear
-            plex_debrid
-            echo -e "\e[1;33mMenu d'options :\e[0m"
-            for i in "${!script_names[@]}"; do
-              echo -e "\e[1;33m$((i + 1)).\e[0m \e[1;32m${script_names[i]}.\e[0m"
-            done
+  source "${SETTINGS_SOURCE}/includes/scripts/functions.sh"
+  source "${SETTINGS_SOURCE}/includes/scripts/prerequis.sh"
+  echo -e  "\e[32mL'installation des prérequis est maintenant terminée, relancez ./install.sh "sans sudo"\e[0m"
 
-            read -p "Sélectionnez une option (1-${#scripts[@]}, Q pour revenir au menu général) : " choix
-            if [ "$choix" == "Q" ] || [ "$choix" == "q" ]; then
-              main_menu
-            
-            elif [[ "$choix" =~ ^[0-9]+$ && "$choix" -ge 1 && "$choix" -le ${#scripts[@]} ]]; then
-              select_file="${scripts[choix - 1]}"
-              path="$scripts_dir/$select_file"
-              echo "Vous avez sélectionné : $path"
-              source $path
-             sleep 10s
-            else
-              echo "Sélection invalide. Appuyez sur Entrée pour continuer."
-              read -r
-            fi
-          done
-          ;;
-        
-        esac
-  done
-}
-clear
-plex_debrid
-main_menu
+fi
