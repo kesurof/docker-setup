bb000ee7639caa71d6bb0a2bd2d19ee961482fde
laster13 <laster13012@gmail.com>
2023-11-15 15:36:20 +0100

    Update functions.sh

--- includes/scripts/functions.sh
+++ includes/scripts/functions.sh
@@ -306,17 +306,3 @@ function manage_apps() {
         esac
 }
 
-function create_folder() {
-echo -e "\e[32mEcrire les noms de dossiers à créer dans Medias ex: Films Series\e[0m \e[36m(touche Entrée après chaque saisie)\e[0m .. \e[32mpuis stop une fois terminé\e[0m"   				
-while :
-do		
-  read -p "" EXCLUDEPATH
-  mkdir -p /home/$USER/Medias/$EXCLUDEPATH
-  chown -R $USER:$USER /home/$USER/Medias/$EXCLUDEPATH
-  if [[ "$EXCLUDEPATH" = "STOP" ]] || [[ "$EXCLUDEPATH" = "stop" ]]; then
-    rm -rf /home/$USER/Medias/$EXCLUDEPATH
-    break
-  fi
-done
-}
-
