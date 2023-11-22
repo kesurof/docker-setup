b47fcc6bc9f31e2f0e670de46078731377ad7516
laster13 <laster13012@gmail.com>
2023-11-22 13:01:50 +0000

    update zurg

--- includes/scripts/config_setup.sh
+++ includes/scripts/config_setup.sh
@@ -106,6 +106,12 @@ if [ -f "$env_file" ]; then
     # Écrire la configuration rclone dans le fichier rclone.conf en remplaçant {{RD_API_KEY}}
     cat <<EOL > "$rclone_config_file"
 [zurg]
+type = webdav
+url = http://zurg:9999
+vendor = other
+pacer_min_sleep = 0
+
+[zurghttp]
 type = http
 url = http://zurg:9999/http
 no_head = false
